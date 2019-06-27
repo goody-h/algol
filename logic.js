8648626cb7cfb970fce3c526465a577e3e861675
Goodhope Ordu <goodhopeordu@yahoo.com>
2019-06-27 03:37:06 -0700

    initialising

---
+++ logic.js
@@ -0,0 +1,62 @@
+var arr = []
+
+function check(first, minRange) {
+
+    arr = document.getElementById("array").value.split(/\s*,\s*/).map(function (v, i) {
+        return Number.parseInt(v)
+    })
+
+    var highest = 0
+    var startIndex = -1
+    var stopIndex = -1
+
+    var summer = 0
+
+    var changeStop = false
+    var changeCursor = true
+
+    var cursor = -1
+
+    for (let i = 0; i < arr.length; i++) {
+        var e = arr[i];
+
+        summer += e
+
+        if (summer > 0 || (summer > -1 && !minRange)) {
+            highest = Math.max(highest, summer)
+
+            changeStop = summer == highest && selectNewRange(first, minRange, cursor, i, startIndex, stopIndex)
+
+            if (changeCursor) {
+                cursor = i
+                changeCursor = false
+            }
+
+            if (changeStop) {
+                startIndex = cursor
+                stopIndex = i
+            }
+        } else {
+            summer = 0
+            changeCursor = true
+        }
+    }
+
+    document.getElementById("result").innerHTML = `${highest},  form ${startIndex} to ${stopIndex}`
+
+    return highest
+}
+
+function selectNewRange(first, minRange, cursor, i, startIndex, stopIndex) {
+    if (startIndex === -1 || stopIndex === -1) return true;
+
+    if (first) {
+        if (minRange && i - cursor > stopIndex - startIndex - 1) return false
+
+    } else {
+        if (minRange && i - cursor > stopIndex - startIndex) return false
+    }
+
+    return true
+}
+
