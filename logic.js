5bc9465e78b54ac2934ad98877f71e2c00f64e93
Goodhope Ordu <goodhopeordu@yahoo.com>
2019-06-27 12:34:22 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,49 +1,57 @@
-var arr = []
+arr = []
 
 /**
  * Two more parameters were included in the function
- * @param first [Boolean] set to true to get the first occurence of largest sum
- * @param minRange [Boolean] set to true to get the largest sum with smallest range
+ * @param {Boolean} first set to true to get the first occurence of largest sum
+ * @param {Boolean} minRange set to true to get the largest sum with smallest range
  * 
- * @returns [Array] of size 3 of the form [largestSum, startIndex, stopIndex] 
+ * @returns {Array} of size 3 of the form [largestSum, startIndex, stopIndex] 
  *  */
 function check(first, minRange) {
 
-    arr = document.getElementById("array").value.split(/\s*,\s*/).map(function (v, i) {
-        return Number.parseInt(v)
-    })
+arr = document.getElementById("array").value.split(/\s*,\s*/).map(function (v, i) {
+    return Number.parseInt(v)
+})
 
+    // Result variable
     var highest = 0
     var startIndex = -1
     var stopIndex = -1
 
-    var summer = 0
-
+    // Indexing variables  
+    var cursor = -1
     var changeStop = false
     var changeCursor = true
 
-    var cursor = -1
+    // Current sum holder
+    var summer = 0
 
     for (let i = 0; i < arr.length; i++) {
         var e = arr[i];
 
+        // Add value to summer
         summer += e
 
         if (summer > 0 || (summer > -1 && !minRange)) {
+
+            // set result to max value
             highest = Math.max(highest, summer)
 
-            changeStop = summer == highest && selectNewRange(first, minRange, cursor, i, startIndex, stopIndex)
-
+            // change cursor when a new summing range has started
             if (changeCursor) {
                 cursor = i
                 changeCursor = false
             }
+
+            // change result ranges when a new highest has been obtained
+            changeStop = summer == highest && selectNewRange(first, minRange, cursor, i, startIndex, stopIndex)
 
             if (changeStop) {
                 startIndex = cursor
                 stopIndex = i
             }
         } else {
+            // start a new summing range
             summer = 0
             changeCursor = true
         }
@@ -51,10 +59,14 @@ function check(first, minRange) {
 
     document.getElementById("result").innerHTML = `${highest},  form position ${startIndex + 1} to ${stopIndex + 1}`
 
-    return highest
+    return [highest, startIndex, stopIndex]
 }
 
+/**
+ * Check if new summing range should be set 
+ */
 function selectNewRange(first, minRange, cursor, i, startIndex, stopIndex) {
+    // if indexes have not been set return true
     if (startIndex === -1 || stopIndex === -1) return true;
 
     if (first) {
@@ -66,4 +78,3 @@ function selectNewRange(first, minRange, cursor, i, startIndex, stopIndex) {
 
     return true
 }
-
